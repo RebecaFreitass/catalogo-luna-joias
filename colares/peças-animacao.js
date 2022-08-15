3f78016d88974ab25f76944cddc7a153de7b7ae7
rebecasguerri <rebecasguerri@gmail.com>
2022-08-15 16:02:59 -0300

    update

---
+++ colares/peças-animacao.js
@@ -0,0 +1,33 @@
+function over(){
+    document.getElementById("lea").src  ='imagens/Chocker Léa 129,90.jpg'
+}
+function out(img){
+    document.getElementById("lea").src ='imagens/Chocker Léa uso 129,90.jpg'
+}
+
+
+function overNanda(){
+    document.getElementById("nanda").src  ='imagens/Chocker Nanda 117,00.jpg'
+}
+function outNanda(img){
+    document.getElementById("Nanda").src ='imagens/Chocker Léa uso 129,90.jpg'
+}
+
+
+function overLila(){
+    document.getElementById("lila").src  ='imagens/choker lila 139.jpg'
+}
+function outLila(img){
+    document.getElementById("lila").src ='imagens/choker lila uso.jpg'
+}
+
+
+function overElisa(){
+    document.getElementById("elisa").src  ='imagens/conjunto elisa 132.jpg'
+}
+function outElisa(img){
+    document.getElementById("elisa").src ='imagens/conjunto elisa uso  132.jpg'
+}
+
+
+
